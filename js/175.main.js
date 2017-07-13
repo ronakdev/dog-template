9309e458aff9b08ac1f85f11ca4a6e1f831f3b9f
Kim Koga <kakoga@users.noreply.github.com>
2017-07-13 13:18:12 -0700

    add form bot security code

--- js/175.main.js
+++ js/175.main.js
@@ -3,3 +3,13 @@
 //
 // Learn more here: https://developer.zesty.io/docs/code-editor/javascript-files/
 
+$(function() {
+	setTimeout(function() {
+    $('form[data-zlf]').each(function() {
+        var $form = $(this),
+            formName = $form.attr('data-zlf');
+        $form.prepend('<input type="hidden" name="zlf" value="'+formName+'" />');
+        $form.prepend('<input type="hidden" name="zcf" value="1" />'); 
+    });
+    },3000);
+});
